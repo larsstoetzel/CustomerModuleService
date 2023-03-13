ae5f2c02bc317c839bbf28e8b6939425ba83dd6b
Lars Stötzel <lars.stoetzel@dialog.ch>
2023-03-13 06:38:08 +0100

    Version 1.02

--- Views/CreateEditCustomerView.xaml.cs
+++ Views/CreateEditCustomerView.xaml.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace CustomerModuleService.Views
+namespace CustomerModules.Views
 {
     /// <summary>
     /// Interaktionslogik für CreateEditCustomerView.xaml
     /// </summary>
     public partial class CreateEditCustomerView : Window
     {
-        public CreateEditCustomerView()
+        public CreateEditCustomerView(int? id = null)
         {
             InitializeComponent();
+        }
+
+
+        private void lbx_ModuleList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void tbx_Url_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
--- Views/CustomerListView.xaml.cs
+++ Views/CustomerListView.xaml.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Navigation;
 
-namespace CustomerModuleService.Views
+namespace CustomerModules.Views
 {
     /// <summary>
-    /// Interaktionslogik für CustomerListView.xaml
+    /// Interaction logic for CustomerListView.xaml
     /// </summary>
     public partial class CustomerListView : Window
     {
+
         public CustomerListView()
         {
+            InitializeComponent();
+
         }
+
+       
     }
 }
